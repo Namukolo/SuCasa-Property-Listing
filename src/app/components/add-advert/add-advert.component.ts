414ed69e801ac5894517d0a2a9e579cef68b14b0
Namukolo <namukolodev@gmail.com>
2021-11-15 11:49:22 +0200

    Binded add-advert template to its form Model

--- src/app/components/add-advert/add-advert.component.ts
+++ src/app/components/add-advert/add-advert.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
@@ -9,12 +10,26 @@ import { AuthenticationService } from 'src/app/services/authentication.service';
 })
 export class AddAdvertComponent implements OnInit {
 
-  constructor(private authenticationService: AuthenticationService, private router: Router) { }
+  advertForm: FormGroup;
+
+  constructor(private authenticationService: AuthenticationService, private router: Router,private fb: FormBuilder, private route: ActivatedRoute,) { }
 
   ngOnInit(): void { 
     if(!this.authenticationService.getLoggedInUser()){
         this.router.navigate(['/login'])
     }
+
+    this.advertForm = this.fb.group({
+      headline: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(100)]],
+      province: ['', Validators.required],
+      city: ['', Validators.required],
+      description: ['', [Validators.required,Validators.minLength(10), Validators.maxLength(1000)]],
+      price: ['', [Validators.min(10000), Validators.max(100000000)]]  
+    });
   }
 
+
+  saveAdvert(){
+    console.log(this.advertForm.value)
+  }
 }
